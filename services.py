fec078afd922b0c17d1b70725297fec4fa56e1e2
LegitBaboulinet <pro@lucasmacori.fr>
2020-06-05 20:40:51 +0200

    (update) Utilisation de systemctl

--- services.py
+++ services.py
@@ -1,15 +1,57 @@
 from flask import jsonify
-import psutil
+from classes.service import Service
 import response
 import subprocess
 
+#
+# Méthodes de module
+#
+
 def list_services(name = None):
+    '''
+    Liste les services activés sur le système
+    @param name: Nom de service à rechercher. Ce paramètre est optionnel. S'il n'est pas renseigné, tous les services sont renvoyés
+    '''
+    # Récupération des services via systemctl
+    p = subprocess.Popen(['systemctl', '--type=service', '--all', '--no-legend', '--no-pager'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, err = p.communicate()
+
+    # Découpage des résultats
+    output = str(output)
+    raw_services = []
+    splited_output = output.replace('\\n', ' ').split()
+    for i in range(0, len(splited_output), 1):
+        element = str(splited_output[i])
+        if element.endswith('.service'):
+            if i > 0:
+                # Le service est découpé, ajout à la liste et passage au suivant
+                current_service['name'] = current_service['name'][0:-len('.service')]
+                current_service['description'] = current_service['description'][1:]
+                raw_services.append(current_service)
+            count = 0
+            current_service = { 'name': element, 'description': '' }
+        else:
+            # Récupération des différents champs
+            if count == 0:
+                current_service['loaded'] = (element == 'loaded')
+            elif count == 1:
+                current_service['active'] = (element == 'active')
+            elif count == 2:
+                current_service['sub'] = element
+            else:
+                current_service['description'] = current_service['description'] + ' ' + element
+            count += 1
+
+    # Tri des services
     services = []
-    # TODO: Utiliser la commande 'systemctl list-unit-files' à la place. Supprimer psutil
-    for proc in psutil.process_iter(['name', 'pid']):
-        if (name == None or name in str(proc.info['name'])):
-            services.append(proc.info)
+    for service in raw_services:
+        if (name == None or name in str(service['name'])):
+            services.append(service)
     return services
+
+#
+# Controlleurs
+#
 
 def search_services(name = None):
     try:
